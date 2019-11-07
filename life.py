ca0ce733e3b0d87f62d401a003e4fd53032d6217
garza <danni900g@gmail.com>
2019-11-07 16:51:05 -0600

    Gráficos implementados, Funcionalidad en botones añadida

--- life.py
+++ life.py
@@ -1,4 +1,4 @@
-#import pygame
+import pygame
 import csv
 
 ##REGLAS
@@ -62,7 +62,7 @@ class Controller:
             for i in range(len(matriz)):
                 for j in range(len(matriz[0])):
                     matriz[i][j] = int(matriz[i][j])
-            self.printMat(matriz)
+           #self.printMat(matriz)
             print("-----------------section------------------")
             return matriz
 
@@ -83,7 +83,7 @@ class Controller:
             for j in range(num):
                 row += [0]
             matriz += [row]
-        self.printMat(matriz)
+        #self.printMat(matriz)
         return matriz
 
     ##Funcion para imprimir una matriz en consola
@@ -135,9 +135,6 @@ class Controller:
             elif  self.getNum(1,lista)>= 3 or self.getNum(2,lista)>=3 or self.getNum(3,lista)>=3:
                 m[x][y] = 3
 
-        #if m[x][y] == 0:
-           # if getNum(1,lista)>= 3 or getNum(2,lista)>=3 or getNum(3,lista)>=3:
-              #  m[x][y] = 3
 
         elif m[x][y] == 2:
             if self.getNum(1,lista) >= 6:
@@ -145,9 +142,7 @@ class Controller:
         elif m[x][y] == 1:
             if self.getNum(2,lista) == 0 or self.getNum(3,lista) == 0:
                 m[x][y] = 0
-        #if m[x][y] == 0:
-          #  if getNum(1,lista) >= 2 and getNum(3,lista) >= 1:
-            #    m[x][y] = 3
+
         return m
 
 
@@ -167,46 +162,44 @@ class Controller:
 #print(getNum(1,lista))
 #turn(matriz)
 
-controller = Controller()
-controller.saveToDisk(controller.turn(matriz))
-controller.init()
-controller.saveToDisk(controller.turn(matriz))
-controller.init()
-controller.saveToDisk(controller.turn(matriz))
-controller.init()
+#controller = Controller()
+#controller.saveToDisk(controller.turn(matriz))
+#controller.init()
+#controller.saveToDisk(controller.turn(matriz))
+#controller.init()
+#controller.saveToDisk(controller.turn(matriz))
+#controller.init()
 
 
-##Ejemplo movimiento de un cubo
+
+##Clase Mouse
+
+class Cursor(pygame.Rect):
+    def __init__(self):
+        pygame.Rect.__init__(self,0,0,1,1)
+    def update(self):
+        self.left,self.top = pygame.mouse.get_pos()
 
 
-#pygame.init()
-#win = pygame.display.set_mode(500,500)
-#x = 50
-#y = x
-#width = 40
-#height = 60
-#vel = 5
-#run = True
-#while run:
-#    pygame.time.delay(100)
-#    for event in pygame.event.get():
-#        if event.type == pygame.QUIT:
-#            run = False      
-#    keys = pygame.key.get_pressed()
-#    if keys[pygame.K_LEFT]:
-#        x -= vel
-#    if keys[pygame.K_RIGHT]:
-#        x += vel
-#    if keys[pygame.K_UP]:
-#        y -= vel
-#    if keys[pygame.K_DOWN]:
-#        y += vel
+class Button(pygame.sprite.Sprite):
+    def __init__(self,image1,image2,x=100,y=100):
+        self.image_n = image1
+        self.image_s = image2
+        self.image_a = self.image_n
+        self.rect = self.image_a.get_rect()
+        self.rect.left,self.rect.top = (x,y)
+    def update(self,window,cursor):
+        if cursor.colliderect(self.rect):
+            self.image_a = self.image_s
+        else:
+            self.image_a = self.image_n
 
-#    win.fill((0,0,0))
-#    pygame.draw.rect(win (255,0,0) , (x,y,width,height))
-#    pygame.display.update()
+        window.blit(self.image_a , self.rect)
 
-#pygame.quit()
+        
+        
+
+
 
 
 
@@ -214,27 +207,27 @@ controller.init()
 # Definimos algunos colores
 
 ##Variable global para controlar lógica de la matriz en cualquier lugar
-#global controller
-#controller = Controller()
+global controllerA
+controllerA = Controller()
 
-#NEGRO = (0, 0, 0)
-#BLANCO = (255, 255, 255)
-#AZUL = ( 0, 255, 255)
-#ROJO = (255, 0, 0)
-#SILVER = (192,192,192)
-#MAROON = (128,0,0)
-#AQUA = (0,255,255) 
-#YELLOW = (255,255,0)
+NEGRO = (0, 0, 0)
+BLANCO = (255, 255, 255)
+AZUL = ( 0, 255, 255)
+ROJO = (255, 0, 0)
+SILVER = (192,192,192)
+MAROON = (128,0,0)
+AQUA = (0,255,255) 
+YELLOW = (255,255,0)
 
  
 # Establecemos el LARGO y ALTO de cada celda de la retícula.
 
-#LARGO  = 20
-#ALTO = 20
+LARGO  = 20
+ALTO = 20
  
 # Establecemos el margen entre las celdas.
 
-#MARGEN = 5
+MARGEN = 5
  
 # Creamos un array bidimensional. Un array bidimensional
 # no es más que una lista de listas.
@@ -247,79 +240,157 @@ controller.init()
     #grid.append([])
     #for columna in range(10):
         #grid[fila].append(0) # Añade una celda
+global grid2
+grid2 = controllerA.init()
 
-#grid2 = controller.makeMat(25)
 
- 
-# Establecemos la fila 1, celda 5 a uno. (Recuerda, los números de las filas y
-# columnas empiezan en cero.)
-#grid2[1][5] = 0
  
 # Inicializamos pygame
-#pygame.init()
+pygame.init()
   
 # Establecemos el LARGO y ALTO de la pantalla
-#DIMENSION_VENTANA = [500, 500]
-#pantalla = pygame.display.set_mode(DIMENSION_VENTANA)
+DIMENSION_VENTANA = [800, 640]
+window = pygame.display.set_mode(DIMENSION_VENTANA)
  
 # Establecemos el título de la pantalla.
-#pygame.display.set_caption("Life")
+pygame.display.set_caption("Life")
  
 # Iteramos hasta que el usuario pulse el botón de salir.
-#hecho = False
+hecho = False
  
 # Lo usamos para establecer cuán rápido de refresca la pantalla.
-#reloj = pygame.time.Clock()
+reloj = pygame.time.Clock()
  
 # -------- Bucle Principal del Programa-----------
-#cont = 0
-#while not hecho:
-    #for evento in pygame.event.get(): 
-        #if evento.type == pygame.QUIT: 
-            #hecho = True
-        #elif evento.type == pygame.MOUSEBUTTONDOWN:
-            # El usuario presiona el ratón. Obtiene su posición.
-            #pos = pygame.mouse.get_pos()
+cont = 0
+
+# Imagenes
+save1 = pygame.image.load("images/save3.png")
+save2 = pygame.image.load("images/save4.png")
+play = pygame.image.load("images/play.png")
+play2 = pygame.image.load("images/play2.png")
+pause = pygame.image.load("images/pause.png")
+pause2 = pygame.image.load("images/pause2.png")
+nexxt = pygame.image.load("images/next.png")
+nexxt2 = pygame.image.load("images/next2.png")
+previous = pygame.image.load("images/previous.png")
+previous2 = pygame.image.load("images/previous2.png")
+
+reload =  pygame.image.load("images/reload.png")
+#imagenesObjetos
+lot = pygame.image.load("images/lote.png")
+house = pygame.image.load("images/casa.png")
+water = pygame.image.load("images/agua.png")
+light = pygame.image.load("images/luz.png")
+
+# Botones
+buttonSave = Button(save1,save2,680,50)
+
+buttonPrevious = Button(previous,previous2,630,150)
+buttonPlay = Button(play,play2,680,150)
+buttonNext = Button(nexxt,nexxt2,725,150)
+buttonReload = Button(reload,reload,680,250)
+
+cursor1 = Cursor()
+
+while not hecho:
+    
+    for evento in pygame.event.get(): 
+        if evento.type == pygame.QUIT: 
+            hecho = True
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            if cursor1.colliderect(buttonSave.rect):
+                controllerA.printMat(grid2)
+                controllerA.saveToDisk(grid2)
+                
+            elif cursor1.colliderect(buttonPrevious.rect):
+                print("Previous")
+            elif cursor1.colliderect(buttonPlay.rect):
+                print("Play")
+            elif cursor1.colliderect(buttonNext.rect):
+                 print("Next")
+                 grid2 = controllerA.turn(grid2)
+                 
+            elif cursor1.colliderect(buttonReload.rect):
+                
+                newGrid = controllerA.makeMat(25)
+                controllerA.saveToDisk(newGrid)
+                grid2 = controllerA.init()
+                
+             #El usuario presiona el ratón. Obtiene su posición.
+            pos = pygame.mouse.get_pos()
             # Cambia las coordenadas x/y de la pantalla por coordenadas reticulares
-            #columna = pos[0] // (LARGO + MARGEN)
-            #fila = pos[1] // (ALTO + MARGEN)
+            columna = pos[0] // (LARGO + MARGEN)
+            fila = pos[1] // (ALTO + MARGEN)
+            
             # Establece esa ubicación a cero
-            #grid2[fila][columna] = cont
-            #cont+=1
-            #if cont > 3:
-                #cont = 0
+            if columna < 25 and fila < 25:
+                grid2[fila][columna] = cont
+                cont+=1
+            if cont > 3:
+                cont = 0
             
             #print("Click ", pos, "Coordenadas de la retícula: ", fila, columna)
+            
+    cursor1.update()
+    window.fill(NEGRO)
+    buttonSave.update(window,cursor1)
+    buttonPlay.update(window,cursor1)
+    buttonNext.update(window,cursor1)
+    buttonPrevious.update(window,cursor1)
+    buttonReload.update(window,cursor1)
+
  
     # Establecemos el fondo de pantalla.
-    #pantalla.fill(NEGRO)
+    
  
     # Dibujamos la retícula
-    #for fila in range(25):
-       # for columna in range(25):
-            #color = BLANCO
-            #if grid2[fila][columna] == 0:
-                #color = SILVER
-            #elif grid2[fila][columna] == 1:
-                #color = MAROON
-            #elif grid2[fila][columna] == 2:
-                #color = AQUA
-            #elif grid2[fila][columna] == 3:
-                #color = YELLOW
-            #pygame.draw.rect(pantalla,
-                             #color,
-                            # [(MARGEN+LARGO) * columna + MARGEN,
-                              #(MARGEN+ALTO) * fila + MARGEN,
-                             # LARGO,
-                              #ALTO])
+    for fila in range(25):
+        for columna in range(25):
+            color = BLANCO
+            #image = lot
+            if grid2[fila][columna] == 0:
+                color = SILVER
+               # image = lot
+            elif grid2[fila][columna] == 1:
+                color = MAROON
+                #image = house
+            elif grid2[fila][columna] == 2:
+                color = AQUA
+                #image = water
+            elif grid2[fila][columna] == 3:
+                color = YELLOW
+                #image = light
+            pygame.draw.rect(window,
+                             color,
+                             [(MARGEN+LARGO) * columna + MARGEN,
+                             (MARGEN+ALTO) * fila + MARGEN,
+                              LARGO,
+                             ALTO])
+            
+          #  pygame.Surface.blit(image, window,  [(MARGEN+LARGO) * columna + MARGEN,
+             #                 (MARGEN+ALTO) * fila + MARGEN,
+                #              LARGO,
+                   #           ALTO])
      
     # Limitamos a 60 fotogramas por segundo.
-    #reloj.tick(60)
+    reloj.tick(60)
  
     # Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
-    #pygame.display.flip()
+    pygame.display.flip()
      
 # Pórtate bien con el IDLE.
-#pygame.quit()
+pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
 
 
